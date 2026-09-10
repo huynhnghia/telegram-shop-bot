9d687f75aed27959544ca22f037170ab51e6e787
huynhnghia <tuanpg2511vn123@gmail.com>
2026-09-10 12:16:29 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -6,17 +6,17 @@ module.exports = {
 
     // Bank config for VietQR
     BANK: {
-        BIN: process.env.BANK_BIN || '970422',
+        BIN: process.env.BANK_BIN || '3838396852',
         ACCOUNT: process.env.BANK_ACCOUNT || '',
         ACCOUNT_NAME: process.env.BANK_ACCOUNT_NAME || '',
-        NAME: process.env.BANK_NAME || 'MB',
+        NAME: process.env.BANK_NAME || 'TechComBank',
     },
 
     BANK2: process.env.BANK2_ACCOUNT ? {
-        BIN: process.env.BANK2_BIN || '970436',
+        BIN: process.env.BANK2_BIN || '0783803044',
         ACCOUNT: process.env.BANK2_ACCOUNT,
         ACCOUNT_NAME: process.env.BANK2_ACCOUNT_NAME || '',
-        NAME: process.env.BANK2_NAME || 'VCB',
+        NAME: process.env.BANK2_NAME || 'MB',
     } : null,
 
     // Payment
@@ -24,6 +24,6 @@ module.exports = {
     SEPAY_API_KEY: process.env.SEPAY_API_KEY || '',
 
     // Shop
-    SHOP_NAME: process.env.SHOP_NAME || 'Starizzi Shop',
-    SUPPORT_CONTACT: process.env.SUPPORT_CONTACT || '@starizzi_support',
+    SHOP_NAME: process.env.SHOP_NAME || 'Shop Bán Hàng Tự Động',
+    SUPPORT_CONTACT: process.env.SUPPORT_CONTACT || '@kcdmh2',
 };
